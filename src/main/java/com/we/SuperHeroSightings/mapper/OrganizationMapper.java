dc9197bed414b50822aa59dd676a9dfc02aa5ab2
Sophia Zhou <102894133+sbagel@users.noreply.github.com>
2023-07-18 08:06:02 -0700

    complete organization mapper

---
+++ src/main/java/com/we/SuperHeroSightings/mapper/OrganizationMapper.java
@@ -0,0 +1,22 @@
+package com.we.SuperHeroSightings.mapper;
+
+import com.we.SuperHeroSightings.entities.Organization;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class OrganizationMapper implements RowMapper<Organization> {
+    @Override
+    public Organization mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Organization org = new Organization();
+        org.setId(rs.getInt("OrganizationPK"));
+        org.setName(rs.getString("OrganizationName"));
+        org.setType(rs.getString("Type"));
+        org.setDescription(rs.getString("Description"));
+        org.setAddress(rs.getString("OrganizationAddress"));
+        org.setPhone(rs.getString("Phone"));
+        org.setContact(rs.getString("ContactInfo"));
+        return org;
+    }
+}
