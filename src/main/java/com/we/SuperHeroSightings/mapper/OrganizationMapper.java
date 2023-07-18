650cf79b32b73c65743afc7af91732a8f900f523
Sophia Zhou <102894133+sbagel@users.noreply.github.com>
2023-07-18 08:55:41 -0700

    fix organization mapper to include members

--- src/main/java/com/we/SuperHeroSightings/mapper/OrganizationMapper.java
+++ src/main/java/com/we/SuperHeroSightings/mapper/OrganizationMapper.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class OrganizationMapper implements RowMapper<Organization> {
     @Override
@@ -17,6 +18,7 @@ public class OrganizationMapper implements RowMapper<Organization> {
         org.setAddress(rs.getString("OrganizationAddress"));
         org.setPhone(rs.getString("Phone"));
         org.setContact(rs.getString("ContactInfo"));
+        org.setMembers(new ArrayList<>());
         return org;
     }
 }
