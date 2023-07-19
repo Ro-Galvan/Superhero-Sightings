20bc4c1f2d75b8db4d1b308d833817e1450639d1
Natalie Tang <natalietang77@gmail.com>
2023-07-19 16:08:21 -0500

    added location service layer + started controller

---
+++ src/main/java/com/we/SuperHeroSightings/controller/LocationController.java
@@ -0,0 +1,10 @@
+package com.we.SuperHeroSightings.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class LocationController {
+
+}
