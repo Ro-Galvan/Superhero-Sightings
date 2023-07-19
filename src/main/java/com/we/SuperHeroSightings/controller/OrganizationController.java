ab0ffab0a057b18fda037236b78dc1c531085bcd
Sophia Zhou <102894133+sbagel@users.noreply.github.com>
2023-07-19 12:29:23 -0700

    implement method to display organizations on endpoint /org

--- src/main/java/com/we/SuperHeroSightings/controller/OrganizationController.java
+++ src/main/java/com/we/SuperHeroSightings/controller/OrganizationController.java
@@ -5,44 +5,22 @@ import com.we.SuperHeroSightings.entities.Organization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/org")
 public class OrganizationController {
     @Autowired
     OrganizationDao organizationDao;
 
-    @GetMapping("/")
-    public List<Organization> getOrganizations() {
-        return organizationDao.getAllOrganizations();
+    @GetMapping("")
+    public String displayOrganizations(Model model) {
+        List<Organization> organizations = organizationDao.getAllOrganizations();
+        model.addAttribute("organizations", organizations);
+        return "organization";
     }
 
-    @PostMapping("/")
-    public Organization addOrganization(@RequestBody Organization organization) {
-        return organizationDao.addOrganization(organization);
-    }
-
-    @GetMapping("/{id}")
-    public Organization getOrganizationById(@PathVariable("id") int id){
-        return organizationDao.getOrganizationByID(id);
-    }
-
-    @PutMapping("/{id}")
-    public Organization updateOrganizationById(@RequestBody Organization organization){
-        organizationDao.updateOrganization(organization);
-        return organizationDao.getOrganizationByID(organization.getId());
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteOrganizationById(@PathVariable("id") int id){
-        organizationDao.deleteOrganizationByID(id);
-
-        if(organizationDao.getOrganizationByID(id) != null){
-            return new ResponseEntity(HttpStatus.NO_CONTENT);
-        }
-
-        return new ResponseEntity(HttpStatus.NOT_FOUND);
-    }
 }
