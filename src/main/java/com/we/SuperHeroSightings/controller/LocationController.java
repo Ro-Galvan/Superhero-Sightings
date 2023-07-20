7ae3c31ed5e9e87549922cd5cdcacf680b0deb40
Natalie Tang <natalietang77@gmail.com>
2023-07-20 16:19:16 -0500

    changed controller + html

--- src/main/java/com/we/SuperHeroSightings/controller/LocationController.java
+++ src/main/java/com/we/SuperHeroSightings/controller/LocationController.java
@@ -2,23 +2,17 @@ package com.we.SuperHeroSightings.controller;
 
 import com.we.SuperHeroSightings.entities.Hero;
 import com.we.SuperHeroSightings.entities.Location;
-import com.we.SuperHeroSightings.entities.Organization;
 import com.we.SuperHeroSightings.service.HeroService;
 import com.we.SuperHeroSightings.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import java.util.HashSet;
+import javax.validation.Valid;
 import java.util.List;
-import java.util.Set;
 
 @Controller
 public class LocationController {
@@ -29,109 +23,55 @@ public class LocationController {
     @Autowired
     HeroService heroService;
 
-    //sets to store ConstraintViolation objects for validation errors
-    Set<ConstraintViolation<Location>> violations = new HashSet<>();
-
     //method handles HTTP GET requests to "/locations"
     @GetMapping("locations")
     public String displayLocations(Model model) {
-        //gets all locations from locationService
         List<Location> locations = locationService.getAllLocations();
-        //adds list of locations as an attribute to model for use in view
         model.addAttribute("locations", locations);
-        //initializes violations as empty HashSet
-        violations = new HashSet<>();
-        //returns view "locations" to display list of locations
         return "locations";
     }
 
     //method handles HTTP GET requests to "/addLocation"
     @GetMapping("addLocation")
     public String displayAddLocation(Model model) {
-        //adds empty location object as an attribute to model for use in view
-        model.addAttribute("location", new Location());
-        //adds violations set as an attribute to model for displaying validation errors in view
-        model.addAttribute("errors", violations);
-        //returns view "addLocation" to display the form for adding new location
+        Location location = new Location();
+        model.addAttribute("location", location);
         return "addLocation";
     }
 
     //method handles HTTP POST requests to "/addLocation"
     @PostMapping("addLocation")
-    public String addLocation(HttpServletRequest request, Model model) {
-        //creates new location object and sets its attributes from data received from the form
-        Location location = new Location();
-        location.setName(request.getParameter("name"));
-        location.setDescription(request.getParameter("description"));
-        location.setAddress(request.getParameter("address"));
-        location.setLatitude(request.getParameter("latitude"));
-        location.setLongitude(request.getParameter("longitude"));
-
-        //adds newly created location object as an attribute to model for use in view
-        model.addAttribute("location", location);
-
-        //validates location object using Bean Validation API and stores ay constraint violations in violations set
-        Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
-        violations = validate.validate(location);
-
-        //if there are no validation errors, adds location to service and redirects user to "/locations"
-        if(violations.isEmpty()) {
-            locationService.addLocation(location);
-            return "redirect:/locations";
+    public String addLocation(@Valid Location location, BindingResult result) {
+        if(result.hasErrors()) {
+            return "addLocation";
         }
 
-        //if there are errors, redirects user back to "/addLocation" to display errors and correct the form
-        return "redirect:/addLocation";
+        locationService.addLocation(location);
+        return "redirect:/locations";
     }
 
     //method handles HTTP GET requests to "/editLocation"
     @GetMapping("editLocation")
-    public String displayEditLocation(HttpServletRequest request, Model model) {
-        //parses 'id' parameter from request URL to identify location to edit
-        int id = Integer.parseInt(request.getParameter("id"));
-        //gets location with specified id from locationService
+    public String displayEditLocation(Integer id, Model model) {
         Location location = locationService.getLocationById(id);
-        //adds location as an attribute to the model for use in view
         model.addAttribute("location", location);
-        //adds violations set as an attribute to model for displaying validation errors in view
-        model.addAttribute("errors", violations);
-        //return view "editLocation" to display the form for editing location
         return "editLocation";
     }
 
     //method handles HTTP POST requests to "/editLocation"
     @PostMapping("editLocation")
-    public String editLocation(HttpServletRequest request) {
-        //parses 'id' parameter from request URL to identify location to edit
-        int id = Integer.parseInt(request.getParameter("id"));
-        //gets location with specified id from locationService
-        Location location = locationService.getLocationById(id);
-        //updates location attributes with data received from the form
-        location.setName(request.getParameter("name"));
-        location.setDescription(request.getParameter("description"));
-        location.setAddress(request.getParameter("address"));
-        location.setLatitude(request.getParameter("latitude"));
-        location.setLongitude(request.getParameter("longitude"));
-
-        //validates location object using Bean Validation API and stores ay constraint violations in violations set
-        Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
-        violations = validate.validate(location);
-
-        //if there are no validation errors, adds location to service and redirects user to "/locations"
-        if(violations.isEmpty()) {
-            locationService.updateLocation(location);
-            return "redirect:/locations";
+    public String editLocation(@Valid Location location, BindingResult result) {
+        if(result.hasErrors()) {
+            return "editLocation";
         }
 
-        //if there are errors, redirects user back to "/editLocation" to display errors and correct the form
-        return "redirect:/editLocation?id="+id;
+        locationService.updateLocation(location);
+        return "redirect:/locations";
     }
 
     //method handles HTTP GET requests to "/deleteLocation"
     @GetMapping("deleteLocation")
-    public String deleteLocation(HttpServletRequest request) {
-        //parses 'id' parameter from request URL to identify the location to delete
-        int id = Integer.parseInt(request.getParameter("id"));
+    public String deleteLocation(Integer id) {
         //deleted the location with the specified id from the locationService
         locationService.deleteLocationById(id);
         //redirects user back to locations list after deleting the location
