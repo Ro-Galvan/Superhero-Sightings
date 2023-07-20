7567904ff7cb54ffc027c27de3901be474b1b498
Sophia Zhou <102894133+sbagel@users.noreply.github.com>
2023-07-19 19:10:46 -0700

    add a constraintViolation tracker to organization controller, as well as an addedObj to save user input when adding a new organization

--- src/main/java/com/we/SuperHeroSightings/controller/OrganizationController.java
+++ src/main/java/com/we/SuperHeroSightings/controller/OrganizationController.java
@@ -11,27 +11,40 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 public class OrganizationController {
     @Autowired
     OrganizationService service;
 
+    Set<ConstraintViolation<Organization>> violations = new HashSet<>();
+
+    Organization addedObj;
+
     @GetMapping("organizations")
     public String displayOrganizations(Model model) {
         List<Organization> organizations = service.getAllOrganizations();
         model.addAttribute("organizations", organizations);
+        violations = new HashSet<>();
+        addedObj = new Organization();
         return "organization";
     }
 
     @GetMapping("/organizations/add")
-    public String displayAddOrganization() {
+    public String displayAddOrganization(Model model) {
+        model.addAttribute("errors", violations);
+        model.addAttribute("addedObj", addedObj);
         return "addOrganization";
     }
 
     @PostMapping("/organizations/add")
-    public String addOrganization(HttpServletRequest request){
+    public String addOrganization(HttpServletRequest request, Model model){
         Organization org = new Organization();
         org.setName(request.getParameter("name"));
         org.setType(request.getParameter("type"));
@@ -39,16 +52,27 @@ public class OrganizationController {
         org.setAddress(request.getParameter("address"));
         org.setPhone(request.getParameter("phone"));
         org.setContact(request.getParameter("contact"));
-        service.addOrganization(org);
+        addedObj = org;
+        model.addAttribute("addedObj", addedObj);
 
-        return "redirect:/organizations";
+        Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
+        violations = validate.validate(org);
+
+        if(violations.isEmpty()){
+            service.addOrganization(org);
+            return "redirect:/organizations";
+        }
+
+        return "redirect:/organizations/add";
     }
 
     @GetMapping("/organizations/edit")
     public String displayEditOrganization(HttpServletRequest request, Model model){
         int id = Integer.parseInt(request.getParameter("id"));
         Organization organization = service.getOrganizationByID(id);
-
+        System.out.print("---------");
+        System.out.print(organization);
+        System.out.print("---------");
         model.addAttribute("organization", organization);
         return "editOrganization";
     }
@@ -63,6 +87,7 @@ public class OrganizationController {
         org.setAddress(request.getParameter("address"));
         org.setPhone(request.getParameter("phone"));
         org.setContact(request.getParameter("contact"));
+
         service.updateOrganization(org);
 
         return "redirect:/organizations";
